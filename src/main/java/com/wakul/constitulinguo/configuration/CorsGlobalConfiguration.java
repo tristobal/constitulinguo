5775f4ba73544fc3901d9b19d6c3a41ad8ff5431
Cristóbal Sánchez <cristobal.sanchez@globant.com>
2020-07-14 12:55:33 -0400

    Agrega CorsGlobalConfiguration

---
+++ src/main/java/com/wakul/constitulinguo/configuration/CorsGlobalConfiguration.java
@@ -0,0 +1,19 @@
+package com.wakul.constitulinguo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class CorsGlobalConfiguration implements WebFluxConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://0.0.0.0:8000")
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+}
